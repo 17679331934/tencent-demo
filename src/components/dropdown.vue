de4aa9a33be11249b63f44e638dd6da49f174e08
nice <57080419+17679331934@users.noreply.github.com>
2023-03-30 21:29:02 +0800

    dropdown

--- src/components/dropdown.vue
+++ src/components/dropdown.vue
@@ -1,5 +1,136 @@
 <template>
-  <div>
-    
+  <div class="dropdown-view" id="dropdown">
+    <div class="dropdown-box" @click="showClick">
+      <span>{{ selectData[current]}}</span>
+      <div class="icon" :class="{'icon-hover' : !isClick}" :style="getIconStyle()"></div>
+      <div></div>
+    </div>
+    <div class="drop-view" :class="{'drop-view-hover' : !isClick}" :style="getDropStyle()">
+      <div class="drop-item" v-for="(item, index) in selectData" :key="index">
+        <input class="drop-radio" type="radio" name="article" @change="setChangeData(item, index)"
+          :id="getHover(index)" @click="current=index" :checked="current==index" hidden>
+        <label class="drop-label" :for="getHover(index)">
+          <span>{{ item }}</span>
+        </label>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    selectData: Array,
+    isClick: Boolean
+  },
+  data() {
+    return {
+      current: 0,
+      isShow: false
+    }
+  },
+  methods: {
+    getHover(value) {
+      return `article${value}`
+    },
+    getDropStyle() {
+      if (this.isShow) {
+        return 'transform: scaleY(1)'
+      } else {
+        return 'transform: scaleY(0)'
+      }
+      
+    },
+    getIconStyle() {
+      if (this.isShow) {
+        return 'transform: rotate(45deg);'
+      } else {
+        return 'transform: rotate(225deg);'
+      }
+    },
+    showClick() {
+      if (!this.isClick) return
+      this.isShow = !this.isShow
+    },
+    setChangeData(value, index) {
+      let res = {value: value, index: index}
+      this.isShow = false
+      this.$emit('changeData', res)
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+  .dropdown-view {
+    padding-left: 10px;
+    position: relative;
+    width: 100px;
+  }
+  .dropdown-box {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    box-sizing: border-box;
+    padding-right: 10px;
+    margin-bottom: 10px;
+  }
+  .icon {
+    border-top: 2px solid #353535;
+    border-left: 2px solid #353535;
+    height: 5px;
+    width: 5px;
+    transform: rotate(225deg);
+    transition: 0.2s;
+  }
+  /* .icon-hover {
+    transform: rotate(45deg);
+  } */
+  .dropdown-box:hover > .icon-hover {
+    transform: rotate(45deg);
+  }
+  .dropdown-box:hover + .drop-view-hover,
+  .drop-view-hover:hover
+   {
+    transform: scaleY(1);
+  }
+  .drop-view {
+    box-shadow: 0 0 5px #525252;
+    height: auto;
+    width: 100px;
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: 10px 0;
+    transform: scaleY(0);
+    transition: 0.2s;
+    transform-origin: top;
+    position: absolute;
+    background-color: #fff;
+  }
+  .drop-item {
+    height: 30px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+  .drop-label {
+    width: 100%;
+    height: 100%;
+    transition: 0.2s;
+    box-sizing: border-box;
+    padding-left: 10px;
+    display: flex;
+    align-items: center;
+  }
+  .drop-label span {
+    position: absolute;
+    transition: .2s;
+  }
+  .drop-label:hover > span {
+    transform: translateX(15px);
+  }
+  .drop-radio:checked + .drop-label {
+    background: #b4b8b4;
+  }
+</style>
--- src/page/home.vue
+++ src/page/home.vue
@@ -4,6 +4,8 @@
       <button @click="handelclick">输出结果</button>
     <h3>面试题二</h3>
     <Count />
+    <h3>面试题三</h3>
+    <x-dropdown :selectData='selectData' @changeData="getchangeData" :isClick="true" />
      <h3>面试题四</h3>
      <router-link to="/activity">activity</router-link>
   </div>
@@ -11,6 +13,7 @@
 
 <script>
 import Count from '../components/Count.vue'
+import dropdown from '../components/dropdown.vue'
 
 import { twoSum } from '../util/answer'
 export default {
@@ -19,16 +22,21 @@ export default {
     return {
       total: 0,
       num: [2, 7, 11, 15],
-      target: 9
+      target: 9,
+      selectData: ['选项1', '选项2', '选项3']
     }
   },
   components: {
-    Count
+    Count,
+    'x-dropdown': dropdown
   },
   methods: {
     handelclick() {
       let rows = twoSum(this.num, this.target);
       alert(rows);
+    },
+    getchangeData(res) {
+      console.log(res)
     }
   }
 }
