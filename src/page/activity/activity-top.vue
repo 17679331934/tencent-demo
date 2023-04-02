82b91b6b8caf780d1120aac999560f2000a1e23a
nice <57080419+17679331934@users.noreply.github.com>
2023-04-02 18:26:17 +0800

    slider-1

--- src/page/activity/activity-top.vue
+++ src/page/activity/activity-top.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="activity-top">
-    <div class="active-top-icon">
+  <div class="activity-slider">
+
+    <div class="activity-top-icon">
       <div class="icon-box-left">
         <img src="../../assets/wangzhelog.png" alt="">
       </div>
@@ -8,23 +9,246 @@
         <img src="../../assets/wangzherenshenright.png" alt="">
       </div>
     </div>
+
+    <div class="slider-info">
+      <div class="info-title">
+        <img src="../../assets/slogan.png" alt="">
+      </div>
+      <div class="info-subsidiary-title">
+        <div>
+          <img src="../../assets/subsidiary-title.png" alt="">
+        </div>
+      </div>
+      <div class="infoname-box">
+        <div class="name">
+          <span>欢迎</span>
+          <span>{{ name }}</span>
+        </div>
+        <div class="check">
+          <span>切换角色</span>
+          <div class="border_bottom"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="zongzi-box">
+      <div class="zongzi-item" v-for="item in 3" :key="item">
+        <div class="pig-box">
+          <div class="boji">
+            <div class="zongzi">
+              <img class="zongzi-img" src="../../assets/zongzi.png" alt="">
+            </div>
+            <img class="boji-img" src="../../assets/boji.png" alt="">
+          </div>
+        </div>
+        <div class="brand-name">
+          <div class="brand-name-text">
+            <span>品牌名称名称</span>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="zongzi-item"></div>
+      <div class="zongzi-item"></div> -->
+    </div>
+
+    <div class="slider-point">
+      <div>
+        <span>
+          不同口味的粽子，不同的惊喜！
+        </span>
+      </div>
+      <div>
+        <span>
+          点击拆开粽子，有机会获得品牌端午粽子礼，端午红包！
+        </span>
+      </div>
+    </div>
+
+    <div class="slider-next">
+      <div class="slider-bottom">
+        <div @click="nextSlider">
+          <img src="../../assets/glide-tip.png" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="atmosphere">
+      <!-- <img src="../../assets/氛围.png" alt=""> -->
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      name: 'xxxx'
+    }
+  },
+  methods: {
+    nextSlider() {
+      // console.log(e)
+      this.$emit('nextSlider')
+    }
+    // orientation() {
+    //   window.addEventListener("orientationchange", function() {
+    //     if(window.orientation === 90){
+    //         console.log('这是竖屏')
+    //       }
+    //       if(window.orientation === 0){
+    //         console.log('这是横屏')
+    //       }
+    //   }, false)
+    // }
+  }
+}
+</script>
+
 
 <style scoped>
-  .active-top-icon {
+  .activity-slider {
+    height: 100%;
+    position: relative;
+    background-image: url('../../assets/slider1-bg(1).png');
+    background-repeat: no-repeat;
+    background-size: 100% 100% ;
+    background-attachment: fixed;
+  }
+  .activity-top-icon {
     display: flex;
     justify-content: space-between;
-    margin-top: 10px
+    padding-top: 10px;
+    
   }
-  .active-top-icon img {
+  .activity-top-icon img {
     height: 20px;
   }
   .icon-box-left {
-    margin-left: 10px;
+    margin-left: 11px;
   }
   .icon-box-right {
+    margin-right: 11px;
+  }
+
+  .slider-info {
+
+  }
+  .slider-info .info-title {
+    display: flex;
+    justify-content: center;
+  }
+  .slider-info .info-title img {
+    height: 10vh;
+  }
+  .slider-info .info-subsidiary-title {
+    margin-top: -1vh;
+  }
+  .slider-info .info-subsidiary-title > div {
+    display: flex;
+    justify-content: center;
+  }
+  .slider-info .info-subsidiary-title img {
+    height: 4.4vh;
+  }
+  .infoname-box {
+    display: flex;
+    justify-content: center;
+    color: #3c7051;
+    font-size: 12px;
+  }
+  .infoname-box {
+    transform: scale(0.8)
+  }
+  .infoname-box .name {
     margin-right: 10px;
   }
+  .infoname-box .check {
+    /* border-bottom: 1px solid #3c7051;
+    transform:scaleY(0.5) */
+  }
+  .infoname-box .border_bottom {
+    width: 100%;
+    height: 1px;
+    background-color: #3c7051;
+    transform: scaleY(0.5)
+  }
+
+  .zongzi-box {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  .pig-box {
+    width: 146px;
+    height: 124px;
+    position: relative;
+  }
+  .zongzi-box .boji .boji-img {
+    width: 146px;
+    position: absolute;
+    bottom: 0;
+  }
+  .zongzi-box .boji .zongzi {
+    width: 146px;
+    height: 124px;
+    text-align: center;
+    position: absolute;
+    z-index: 10;
+  }
+  .zongzi-box .boji .zongzi .zongzi-img {
+    width: 115px;
+  }
+  .brand-name {
+    display: flex;
+    justify-content: center;
+    padding: 6px 0;
+  }
+  .brand-name-text {
+    width: 130px;
+    height: 25px;
+    line-height: 25px;
+    text-align: center;
+    color: #FFF;
+    font-size: 12px;
+    background-image: url('../../assets/brand-bg.png');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+  }
+
+  .slider-point {
+    padding: 10px 0;
+    text-align: center;
+    color: #FFF;
+    font-size: 12px;
+  }
+
+  .slider-next {
+    width: 100%;
+    position: absolute;
+    bottom: 2vh;
+    z-index: 20;
+  }
+  .slider-bottom {
+    display: flex;
+    justify-content: center;
+  }
+  .slider-bottom img {
+    height: 16px;
+  }
+
+  .atmosphere {
+    width: 100%;
+    position: absolute;
+    top:0
+  }
+  .atmosphere {
+    width: 100%;
+    height: 100%;
+    background-image: url('../../assets/氛围.png');
+    background-size: 150% 150%;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position-x: -130px;
+    /* z-index: 20; */
+  }
+
 </style>
--- src/page/activity/activity.vue
+++ src/page/activity/activity.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="activity">
-    <el-row>
-      <el-col :span="24">
-        <div class="grid-content bg-purple-dark">
-          <div class="goBack" @click="goBack"><span>返回</span></div>
-          <div>王者人生</div>
-          <div></div>
-        </div>
-      </el-col>
-    </el-row>
-    <el-carousel height="calc(100vh - 40px)" direction="vertical" :autoplay="false" indicator-position="none" :loop="false" ref="slideCarousel">
-      <el-carousel-item>
-        <activityTop />
-      </el-carousel-item>
-      <el-carousel-item>
-        <activityBottom />
-      </el-carousel-item>
-    </el-carousel>
+    <div v-if="verticalScreen">
+      <el-row>
+        <el-col :span="24">
+          <div class="grid-content bg-purple-dark">
+            <div class="goBack" @click="goBack"><span>返回</span></div>
+            <div class="title">王者人生</div>
+            <div class="right"></div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <div v-else>
+      <div class="Horizontal_goback">
+        <img src="../../assets/gobackLogo.png" alt="">
+      </div>
+    </div>
+    
+    <div class="activity-main">
+      <el-carousel :height="carouselHeight" direction="vertical" :autoplay="false" indicator-position="none" :loop="false" ref="slideCarousel">
+        <el-carousel-item>
+          <activityTop @nextSlider="nextSlider"/>
+        </el-carousel-item>
+        <el-carousel-item>
+          <activityBottom />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
   </div>
 </template>
 
 <script>
+// let VerticalScreen = window.orientation
 import activityTop from './activity-top.vue'
 import activityBottom from './activity-bottom.vue'
 export default {
@@ -29,14 +40,22 @@ export default {
     activityBottom
   },
   data() {
-    return {}
+    return {
+      verticalScreen: true,
+      carouselHeight: 'calc(100vh - 40px)'
+    }
   },
   mounted() {
-      if (this.$refs.slideCarousel) this.slideBanner()
+    // 初始化verticalScreen
+    this.renderOrientati()
+    // 滑块触移
+    if (this.$refs.slideCarousel) this.slideBanner();
+    // 监听页面旋转
+    window.addEventListener("orientationchange", this.renderOrientati, false);
   },  
   methods:{
     slideBanner() {
-      //选中的轮播图
+      //选中的滑块
       let box = document.querySelector('.el-carousel__container');
       let startPoint = 0;
       let stopPoint = 0;
@@ -47,15 +66,15 @@ export default {
       }
       //手指按下
       box.addEventListener("touchstart", function (e) {
-        //手指点击位置的X坐标
+        //手指点击位置的Y坐标
         startPoint = e.changedTouches[0].pageY;
       });
       //手指滑动
       box.addEventListener("touchmove", function (e) {
-        //手指滑动后终点位置X的坐标
+        //手指滑动后终点位置Y的坐标
         stopPoint = e.changedTouches[0].pageY;
       });
-      //当手指抬起的时候，判断图片滚动离左右的距离
+      //当手指抬起的时候，判断滑块滚动离上下的距离
       let that = this
       box.addEventListener("touchend", function () {
         if (stopPoint == 0 || startPoint - stopPoint == 0) {
@@ -74,8 +93,23 @@ export default {
         }
       });
     },
+    nextSlider() {
+      this.$refs.slideCarousel.next();
+    },
+    renderOrientati() {
+      // 竖屏
+      if (window.orientation === 0) {
+        this.verticalScreen = true;
+        this.carouselHeight = 'calc(100vh - 40px)'
+      }
+      // 横屏
+      if (window.orientation === 90) {
+        this.verticalScreen = false;
+        this.carouselHeight = '100vh'
+      }
+    },
     goBack() {
-      this.$router.go(-1)
+      this.$router.go(-1);
     }
   }
 }
@@ -85,28 +119,35 @@ export default {
 <style scoped>
   .grid-content {
     height: 40px;
-    background-color: #475669;
+    background-color: #1a1b2f;
     display: flex;
     align-items: center;
     justify-content: space-between;
   }
   .goBack {
     margin-left: 16px;
+    float: left;
+    width: 100px;
   }
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 200px;
-    margin: 0;
-    
+  .title {
+    color: #fff;
   }
-  
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
+  .right {
+    float: right;
+    width: 100px;
   }
-  
-  .el-carousel__item:nth-child(2n+1) {
+
+  .Horizontal_goback {
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 30;
+  }
+  .Horizontal_goback img {
+    height: 28px;
+  }
+
+  .activity-main {
     background-color: #d3dce6;
   }
 </style>
